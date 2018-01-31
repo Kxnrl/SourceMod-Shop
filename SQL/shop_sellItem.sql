c67c1ab8b5a8d958aa637c80ea5ee23a0a14dfac
Kyle <h673321480@qq.com>
2018-01-31 20:13:59 +0800

    PROCEDURE shop_sellItem

---
+++ SQL/shop_sellItem.sql
@@ -0,0 +1,48 @@
+CREATE PROCEDURE `shop_sellItem`
+(
+IN
+    userId INT(11),
+    dbIndex INT(11),
+    price INT(11),
+    reason VARCHAR(256)
+)
+
+SQL SECURITY INVOKER
+
+BEGIN
+
+DECLARE result_code TINYINT(3);
+
+START TRANSACTION;
+
+    DELETE FROM `dxg_inventory`
+    WHERE
+            `id` = `dbIndex`
+        AND
+            `uid` = `userId`;
+
+    
+
+    IF (ROW_COUNT() <> 0) THEN
+        
+        /* UPDATE Money */
+        UPDATE  `dxg_users`
+        SET     `money` = `money` + `price`
+        WHERE   `id` = `userId`;
+
+        /* LOGGING */
+        INSERT INTO `dxg_banklog`
+        VALUES (DEFAULT, `userId`, `price`, `reason`, UNIX_TIMESTAMP());
+        
+        SET result_code = 0;
+
+    ELSE 
+
+        /* tell plugin failure */
+        SET result_code = -1;
+
+    END IF;
+
+    SELECT price, result_code;
+
+END;

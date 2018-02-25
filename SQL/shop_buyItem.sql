786d3a970ac3554d32db91cb953034531a4bb648
Kyle <h673321480@qq.com>
2018-02-25 18:19:36 +0800

    update sql procedure

--- SQL/shop_buyItem.sql
+++ SQL/shop_buyItem.sql
@@ -8,44 +8,53 @@ IN
     reason VARCHAR(256)
 )
 
-SQL SECURITY INVOKER
-
-START TRANSACTION;
+SQL SECURITY INVOKER BEGIN
 
     DECLARE dbIndex TINYINT(3) DEFAULT -1;
     DECLARE logCost INT(11);
-
-    SET logCost = 0 - `price`;
-
-    /* UPDATE Cost */
-    UPDATE  `dxg_users`
-    SET     `money` = `money` - `price`
-    WHERE   `uid` = `userId`;
-
-    IF (ROW_COUNT() <> 0) THEN
-        
-        /* INSERT Item */
-        INSERT INTO `dxg_inventory`
-        VALUES (DEFAULT, `userId`, `uniqueId`, `price`, UNIX_TIMESTAMP(), `expiration`);
     
-        /* GET INSERT ID */
-        SET dbIndex = LAST_INSERT_ID();
-
-        /* LOGGING */
-        INSERT INTO `dxg_banklog`
-        VALUES (DEFAULT, `userId`, `logCost`, `reason`, UNIX_TIMESTAMP());
-
-        COMMIT;
+    DECLARE EXIT handler FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+            SET dbIndex = -1;
+        END;
     
-    ELSE 
+    DECLARE EXIT handler FOR SQLWARNING
+        BEGIN
+            ROLLBACK;
+            SET dbIndex = -1;
+        END;
 
-        /* rollback */
-        ROLLBACK;
+    START TRANSACTION;
 
-        /* tell plugin failure */
-        SET dbIndex = -1;
+        SET logCost = 0 - `price`;
 
-    END IF;
+        /* UPDATE Cost */
+        UPDATE  `dxg_users`
+        SET     `money` = `money` - `price`
+        WHERE   `uid` = `userId`;
+
+        IF (ROW_COUNT() <> 0) THEN
+            
+            /* INSERT Item */
+            INSERT INTO `dxg_inventory`
+            VALUES (DEFAULT, `userId`, `uniqueId`, `price`, UNIX_TIMESTAMP(), `expiration`);
+
+            /* GET INSERT ID */
+            SET dbIndex = LAST_INSERT_ID();
+
+            /* LOGGING */
+            INSERT INTO `dxg_banklog`
+            VALUES (DEFAULT, `userId`, `logCost`, `reason`, UNIX_TIMESTAMP());
+
+        ELSE 
+
+            /* tell plugin failure */
+            SET dbIndex = -1;
+
+        END IF;
+
+    COMMIT;
 
     SELECT dbIndex, logCost;
 
--- SQL/shop_sellItem.sql
+++ SQL/shop_sellItem.sql
@@ -7,42 +7,51 @@ IN
     reason VARCHAR(256)
 )
 
-SQL SECURITY INVOKER
-
-START TRANSACTION;
+SQL SECURITY INVOKER BEGIN
 
     DECLARE result_code TINYINT(3);
-
-    DELETE FROM `dxg_inventory`
-    WHERE
-            `id` = `dbIndex`
-        AND
-            `uid` = `userId`;
-
-    IF (ROW_COUNT() > 0) THEN
-
-        /* UPDATE Money */
-        UPDATE  `dxg_users`
-        SET     `money` = `money` + `price`
-        WHERE   `uid` = `userId`;
-
-        /* LOGGING */
-        INSERT INTO `dxg_banklog`
-        VALUES (DEFAULT, `userId`, `price`, `reason`, UNIX_TIMESTAMP());
-        
-        COMMIT;
-
-        SET result_code = 0;
-
-    ELSE 
-
-        /* rollback */
-        ROLLBACK;
     
-        /* tell plugin failure */
-        SET result_code = -1;
+    DECLARE EXIT handler FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+            SET result_code = -1;
+        END;
+    
+    DECLARE EXIT handler FOR SQLWARNING
+        BEGIN
+            ROLLBACK;
+            SET result_code = -1;
+        END;
 
-    END IF;
+    START TRANSACTION;
+
+        DELETE FROM `dxg_inventory`
+        WHERE
+                `id` = `dbIndex`
+            AND
+                `uid` = `userId`;
+
+        IF (ROW_COUNT() > 0) THEN
+
+            /* UPDATE Money */
+            UPDATE  `dxg_users`
+            SET     `money` = `money` + `price`
+            WHERE   `uid` = `userId`;
+
+            /* LOGGING */
+            INSERT INTO `dxg_banklog`
+            VALUES (DEFAULT, `userId`, `price`, `reason`, UNIX_TIMESTAMP());
+
+            SET result_code = 0;
+
+        ELSE 
+
+            /* tell plugin failure */
+            SET result_code = -1;
+
+        END IF;
+        
+    COMMIT;
 
     SELECT price, result_code;
 
